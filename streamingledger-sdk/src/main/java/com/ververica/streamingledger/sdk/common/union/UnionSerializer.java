67d5a271587a36ba3aa6fc0ffbd51d263ff5b7b6
Seth Wiesman <sjwiesman@gmail.com>
2019-12-12 10:38:36 -0600

    Update minimum supported Flink version to 1.9

--- streamingledger-sdk/src/main/java/com/ververica/streamingledger/sdk/common/union/UnionSerializer.java
+++ streamingledger-sdk/src/main/java/com/ververica/streamingledger/sdk/common/union/UnionSerializer.java
@@ -16,14 +16,8 @@
 
 package com.ververica.streamingledger.sdk.common.union;
 
-import org.apache.flink.api.common.typeutils.CompatibilityResult;
-import org.apache.flink.api.common.typeutils.CompatibilityUtil;
-import org.apache.flink.api.common.typeutils.TypeDeserializer;
-import org.apache.flink.api.common.typeutils.TypeDeserializerAdapter;
 import org.apache.flink.api.common.typeutils.TypeSerializer;
-import org.apache.flink.api.common.typeutils.TypeSerializerConfigSnapshot;
-import org.apache.flink.api.common.typeutils.UnloadableDummyTypeSerializer;
-import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.common.typeutils.TypeSerializerSnapshot;
 import org.apache.flink.core.memory.DataInputView;
 import org.apache.flink.core.memory.DataOutputView;
 
@@ -33,7 +27,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Objects.requireNonNull;
-import static org.apache.flink.api.common.typeutils.CompatibilityResult.requiresMigration;
 import static org.apache.flink.shaded.curator.org.apache.curator.shaded.com.google.common.base.Preconditions.checkArgument;
 
 final class UnionSerializer extends TypeSerializer<TaggedElement> {
@@ -164,64 +157,22 @@ final class UnionSerializer extends TypeSerializer<TaggedElement> {
     }
 
     @Override
-    public boolean canEqual(Object obj) {
-        return obj instanceof UnionSerializer;
+    public int hashCode() {
+        return Arrays.hashCode(underlyingSerializers);
     }
 
     @Override
-    public int hashCode() {
-        return Arrays.hashCode(underlyingSerializers);
+    public TypeSerializerSnapshot<TaggedElement> snapshotConfiguration() {
+        return new UnionSerializerSnapshot(this);
+    }
+
+    TypeSerializer<?>[] getUnderlyingSerializer() {
+        return this.underlyingSerializers;
     }
 
     // -----------------------------------------------------------------------------------
     // Internal helper methods
     // -----------------------------------------------------------------------------------
-
-    @Override
-    public TypeSerializerConfigSnapshot snapshotConfiguration() {
-        return new UnionSerializerConfigSnapshot(Arrays.asList(underlyingSerializers));
-    }
-
-    @Override
-    public CompatibilityResult<TaggedElement> ensureCompatibility(TypeSerializerConfigSnapshot configSnapshot) {
-        if (!(configSnapshot instanceof UnionSerializerConfigSnapshot)) {
-            return requiresMigration();
-        }
-        UnionSerializerConfigSnapshot config = (UnionSerializerConfigSnapshot) configSnapshot;
-        List<Tuple2<TypeSerializer<?>, TypeSerializerConfigSnapshot>> previousSerializersAndConfigs =
-                config.getNestedSerializersAndConfigs();
-
-        if (previousSerializersAndConfigs.size() != underlyingSerializers.length) {
-            return requiresMigration();
-        }
-
-        List<TypeSerializer<?>> migratedSerializers = new ArrayList<>();
-        // Adopted from the EitherSerializerConfigSnapshot.
-        // One (or more) of the serializers here is not compatible with one (or more) of the previous serializers.
-        boolean requiresMigration = false;
-        for (int i = 0; i < underlyingSerializers.length; i++) {
-            final TypeSerializer<?> underlyingSerializer = underlyingSerializers[i];
-
-            final CompatibilityResult<?> res = CompatibilityUtil.resolveCompatibilityResult(
-                    previousSerializersAndConfigs.get(i).f0,
-                    UnloadableDummyTypeSerializer.class,
-                    previousSerializersAndConfigs.get(i).f1,
-                    underlyingSerializer);
-
-            if (!res.isRequiresMigration()) {
-                migratedSerializers.add(underlyingSerializer);
-                continue;
-            }
-            requiresMigration = true;
-            TypeDeserializer<?> deserializer = res.getConvertDeserializer();
-            if (deserializer == null) {
-                return requiresMigration();
-            }
-            migratedSerializers.add(new TypeDeserializerAdapter<>(deserializer));
-        }
-        UnionSerializer migratedSerializer = new UnionSerializer(migratedSerializers);
-        return requiresMigration ? requiresMigration(migratedSerializer) : CompatibilityResult.compatible();
-    }
 
     private void readObject(java.io.ObjectInputStream in)
             throws IOException, ClassNotFoundException {
